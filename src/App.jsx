e63b416470befd914f23dc235e9594a8ae3a1808
Anderson <syndrax20@gmail.com>
2024-08-04 18:55:30 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <section>
+      <h2 className='text-4xl'>
+        domo
+      </h2>
+    </section>
+  )
+}
+
+export default App
